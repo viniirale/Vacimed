dfc0883d848fbe01c2104fde14a3782ba8296927
viniirale <viniirale@hotmail.com>
2019-11-09 18:01:28 -0300

    iniciando heroku

--- index.php
+++ index.php
@@ -1,3 +1,65 @@
 <?php
-echo "teste";
+/*define('SITE_ROOT', dirname(dirname(__FILE__)));
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+spl_autoload_register(function ($class) {
+    if (file_exists("controller/$class.php")) {
+        require_once "controller/$class.php";
+        return true;
+    }
+});*/
+?>
+<!DOCTYPE html>
+<html lang='pt-br'>
+<header>
+    <meta charset="utf-8">
+    <title>Informações de Medicamentos</title>
+    <!-- Theme CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!-- Bootstrap core JavaScript -->
+    <script src="js/jquery.js"></script>
+    <script src="js/jQuery-Mask-Plugin-master/dist/jquery.mask.min.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/modal.js"></script>
+</header>
 
+<body>
+
+<table class="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+
+</body>
+
+</html>
