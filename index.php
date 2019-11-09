ff29eeaa355a61da453755e8b892fe3027f5f6c3
viniirale <viniirale@hotmail.com>
2019-11-09 19:29:12 -0300

    teste crud

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/*define('SITE_ROOT', dirname(dirname(__FILE__)));
+define('SITE_ROOT', dirname(dirname(__FILE__)));
 error_reporting(E_ALL);
 ini_set('display_errors', true);
 spl_autoload_register(function ($class) {
@@ -7,57 +7,49 @@ spl_autoload_register(function ($class) {
         require_once "controller/$class.php";
         return true;
     }
-});*/
+});
 ?>
 <!DOCTYPE html>
 <html lang='pt-br'>
 <header>
     <meta charset="utf-8">
-    <title>Informações de Medicamentos</title>
+    <title>Agenda de Clientes</title>
     <!-- Theme CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="content/bootstrap.css" rel="stylesheet">
+    <link href="content/bootstrap.min.css" rel="stylesheet">
+    <link href="content/style.css" rel="stylesheet">
     <!-- Bootstrap core JavaScript -->
-    <script src="js/jquery.js"></script>
-    <script src="js/jQuery-Mask-Plugin-master/dist/jquery.mask.min.js"></script>
-    <script src="js/bootstrap.bundle.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/modal.js"></script>
+    <script src="scripts/jquery.js"></script>
+    <script src="scripts/jQuery-Mask-Plugin-master/dist/jquery.mask.min.js"></script>
+    <script src="scripts/bootstrap.bundle.js"></script>
+    <script src="scripts/bootstrap.bundle.min.js"></script>
+    <script src="scripts/modal.js"></script>
 </header>
 
 <body>
 
-<table class="table table-dark">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+    <?php
+    if ($_GET) {
+        $controller = isset($_GET['controller']) ? ((class_exists($_GET['controller'])) ? new $_GET['controller'] : NULL) : null;
+        $method     = isset($_GET['method']) ? $_GET['method'] : null;
+        if ($controller && $method) {
+            if (method_exists($controller, $method)) {
+                $parameters = $_GET;
+                unset($parameters['controller']);
+                unset($parameters['method']);
+                call_user_func(array($controller, $method), $parameters);
+            } else {
+                echo "Método não encontrado!";
+            }
+        } else {
+            echo "Controller não encontrado!";
+        }
+    } else {
+        echo '<div class="container"><h1>Clientes</h1><hr>';
+        echo 'Bem-vindo ao aplicativo da Husky Clientes! <br /><br />';
+        echo '<a href="?controller=ClientsController&method=list" class="btn btn-success">Vamos Começar!</a></div>';
+    }
+    ?>
 
 
 </body>
