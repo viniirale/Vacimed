3f6a0c6f3d5f129a7634ded1f890c318f39fb59e
viniirale <viniirale@hotmail.com>
2019-11-10 12:12:33 -0300

    update

--- header.php
+++ header.php
@@ -5,24 +5,135 @@
 <section>
     <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-index ">
 
-        <div class="collapse navbar-collapse center-navbar " id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item ">
-                    <a class="nav-link navbar-color" href="index.php">Página Inicial</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link navbar-color" href="#">Medicamentos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link navbar-color" href="#">Farmácias</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link navbar-color" href="sobreNos.php">Quem Somos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link navbar-color" href="https://sistema.ouvidorias.gov.br/publico/Manifestacao/SelecionarTipoManifestacao.aspx">Ouvidoria</a>
-                </li>
-            </ul>
-        </div>
+        <?php
+        if ($_SERVER['REQUEST_URI'] == "/index.php") {
+            ?>
+            <div class="collapse navbar-collapse center-navbar " id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item ">
+                        <a class="nav-link navbar-color bold" href="index.php">Página Inicial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="#">Medicamentos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="#">Farmácias</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="sobreNos.php">Quem Somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="https://sistema.ouvidorias.gov.br/publico/Manifestacao/SelecionarTipoManifestacao.aspx">Ouvidoria</a>
+                    </li>
+                </ul>
+            </div>
+        <?php
+        }
+        ?>
+
+        <?php
+        if ($_SERVER['REQUEST_URI'] == "/medicamentos.php") {
+            ?>
+            <div class="collapse navbar-collapse center-navbar " id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item ">
+                        <a class="nav-link navbar-color " href="index.php">Página Inicial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color bold" href="#">Medicamentos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="#">Farmácias</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="sobreNos.php">Quem Somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="https://sistema.ouvidorias.gov.br/publico/Manifestacao/SelecionarTipoManifestacao.aspx">Ouvidoria</a>
+                    </li>
+                </ul>
+            </div>
+        <?php
+        }
+        ?>
+
+        <?php
+        if ($_SERVER['REQUEST_URI'] == "/farmacias.php") {
+            ?>
+            <div class="collapse navbar-collapse center-navbar " id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item ">
+                        <a class="nav-link navbar-color " href="index.php">Página Inicial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="#">Medicamentos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color bold" href="#">Farmácias</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="sobreNos.php">Quem Somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="https://sistema.ouvidorias.gov.br/publico/Manifestacao/SelecionarTipoManifestacao.aspx">Ouvidoria</a>
+                    </li>
+                </ul>
+            </div>
+        <?php
+        }
+        ?>
+
+        <?php
+        if ($_SERVER['REQUEST_URI'] == "/sobreNos.php") {
+            ?>
+            <div class="collapse navbar-collapse center-navbar " id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item ">
+                        <a class="nav-link navbar-color " href="index.php">Página Inicial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="#">Medicamentos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="#">Farmácias</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color bold" href="sobreNos.php">Quem Somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="https://sistema.ouvidorias.gov.br/publico/Manifestacao/SelecionarTipoManifestacao.aspx">Ouvidoria</a>
+                    </li>
+                </ul>
+            </div>
+        <?php
+        }
+        ?>
+
+        <?php
+        if ($_SERVER['REQUEST_URI'] == "/ouvidoria.php") {
+            ?>
+            <div class="collapse navbar-collapse center-navbar " id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item ">
+                        <a class="nav-link navbar-color bold" href="index.php">Página Inicial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="#">Medicamentos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="#">Farmácias</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color" href="sobreNos.php">Quem Somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-color bold" href="https://sistema.ouvidorias.gov.br/publico/Manifestacao/SelecionarTipoManifestacao.aspx">Ouvidoria</a>
+                    </li>
+                </ul>
+            </div>
+        <?php
+        }
+        ?>
+
     </nav>
 </section>
