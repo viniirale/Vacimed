c428d6365ca78ac21bb6cb4237b00fb200dd1200
Luan Aguiar <luan.b.aguiar3@gmail.com>
2019-11-10 10:05:48 -0300

    adicionando sobreNos.php

---
+++ sobreNos.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Sobe Nos</title>
+        <!-- Theme CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <!-- Bootstrap core JavaScript -->
+        <script src="js/jquery.js"></script>
+        <script src="js/jQuery-Mask-Plugin-master/dist/jquery.mask.min.js"></script>
+        <script src="js/bootstrap.bundle.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/modal.js"></script>
+    </head>
+    <body>
+
+        <section class="header-index">
+            <img src="img/logo.png" width="400" alt="Logo" class="img-fluid center">
+
+        </section>
+
+        <section>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-index ">
+
+                <div class="collapse navbar-collapse teste " id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item ">
+                            <a class="nav-link navbar-color" href="index.php">Página Inicial</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link navbar-color" href="medicamentos.php">Medicamentos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link navbar-color" href="#">Farmácias</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link navbar-color" href="sobreNos.php">Quem Somos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link navbar-color" href="https://sistema.ouvidorias.gov.br/publico/Manifestacao/SelecionarTipoManifestacao.aspx">Ouvidoria</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </section>
+
+
+        <section>
+            <div id="info">
+                <h1 id="quemsomos">Quem Somos</h1>
+                <p>Saiba mais sobre o sistema e sua equipe desenvolvedora.</p>
+                <h1 id="nossoprojeto">Nosso Projeto</h1>
+                <p>Inspirados numa PL sobre transparência dos <i>estoques</i> de remédios especiais, nós buscamos criar uma ferramenta que pudesse viabilizar essa ideia.
+
+                Durante o processo de idealização do projeto, percebemos que é recorrente a criação de ferramentas de buscas de remédios e tivemos algumas dificuldades em pensar o que poderíamos fazer de diferente.
+
+                Descobrimos então que existe o <b>BNAFAR (Base Nacional de Dados de Ações e Serviços da Assistência Farmacêutica)</b> com informações sobre o controle de entrada e saída dos recursos farmacêuticos públicos e privados. Com isso pudemos chegar a ideia final: uma aplicação que beneficia o cidadão e as redes farmacêuticas.
+
+                O problema com a BNAFAR é que nem todos os órgãos públicos utilizam ferramentas conectadas com esse web service. O que torna instável a confiabilidade das informações. Com a criação e popularização da ferramenta, esperamos que os cidadãos possam questionar a ausência de certas informações e incentivar o governo a cada vez mais globalizar essas informações.
+
+                Conforme a popularização da ideia, podemos esperar geração de dados estatísticos úteis para que a máquina pública possa gerir melhor seus recursos e otimizá-los de acordo com as necessidades da população.
+                </p>
+            </div>
+
+            <div>
+                <img id="nos" src="img/juntos.png" width="600" alt="vos" class="center" >
+            </div>
+
+
+        </section>
+
+        <section>
+         <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-index ">
+
+                <div class="collapse navbar-collapse teste " id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item ">
+                            <a class="nav-link navbar-color" href="#">Página Inicial</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link navbar-color" href="#">Medicamentos</a>
+                            <small><a class="nav-link navbar-color" href="#">Busca por Código</a></small>
+                            <small><a class="nav-link navbar-color" href="#">Busca por Preço</a></small>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link navbar-color" href="#">Farmácias</a>
+                            <small><a class="nav-link navbar-color" href="#">Busca por Proximidade</a></small>
+                            <small><a class="nav-link navbar-color" href="#">Buscar meu Preço</a></small>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link navbar-color" href="#">Quem Somos</a>
+                            <small><a class="nav-link navbar-color" href="#">O Projeto</a></small>
+                            <small><a class="nav-link navbar-color" href="#">A Equipe</a></small>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link navbar-color" href="https://sistema.ouvidorias.gov.br/publico/Manifestacao/SelecionarTipoManifestacao.aspx">Ouvidoria</a>
+                            <small><a class="nav-link navbar-color" href="#">Sobre a Ouvidoria</a></small>
+                            <small><a class="nav-link navbar-color" href="#">Faça uma Requisição</a></small>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </section>
+
+
+
+    </body>
+</html>
